9d0ab10b22b859d9853581003db00ff94badbdd9
asilverstorm <78556714+asilverstorm@users.noreply.github.com>
2021-02-06 08:18:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ bot.on('message', (message) => {
     let MessageArray = message.content.split(' ');
     let cmd = MessageArray[0].slice(prefix.length);
     let args = MessageArray.slice(1);
-    if(!message.content.startsWith(prefix)) return
+    if(!message.content.startsWith(prefix)) return;
     if(cmd == 'links') {
         message.channel.send(`***Hey, there, <@${message.author.id}>! We have numerous connections for Silver-Studios! Consider giving us a follow on our social media, or donate to us.*** :grin:\n\n:video_game: __Roblox:__ <https://web.roblox.com/groups/5068592/Silver-Studios>\n:bird: __Twitter:__ <https://twitter.com/silverstudios9>\n:red_circle: __YouTube:__ <https://www.youtube.com/channel/UC9jauBokYcaxt5EjTUXPJOA>\n:desktop: __Website:__ <https://silver-studios.xyz>\n:moneybag: __Patreon:__ <https://patreon.com/SilverStudios>\n:e_mail: __Discord Invite:__ <https://dsc.gg/Silver-Studios>\n\n*Feel free to go to our website if you want to buy our merch or any benefits for Discord or our games. People who wish to formally contact us or submit an application are also urged to visit our website.*\n------------------------------------------`);
     }
@@ -54,7 +54,7 @@ bot.on('message', (message) => {
     let MessageArray = message.content.split(' ');
     let cmd = MessageArray[0].slice(prefix.length);
     let args = MessageArray.slice(1);
-    if(!message.content.startsWith(prefix)) return
+    if(!message.content.startsWith(prefix)) return;
     if(cmd == 'rolesinfo') {
         message.channel.send(`***We have many roles for you to apply for, earn, and even give yourself! See a list of each role and what they do below :point_down:***\n\n*Staff Roles*\n-\n<@&727595225123651625>: __You are a friend of the Owner__\n<@&806731297640808448>: __Role given to all staff, including developers__\n<@&805675536130113547>: __You moderate the discord and other group chats we have__\n<@&631316263066664961>: __You moderate the games and are supervisors in the Discord__\n<@&631323272927248384>: __You help program our games, website, or Discord bot__\n\n*Special Roles*\n-\n<@&723353782502817892>: __You have donated once or more in the past__\n<@&631323854593196058>: __You have bought this role from our website__\n<@&723353663770329233>: __You have the standard plan in our Patreon__\n<@&723356769878802494>: __You have the premium plan in our Patreon__\n\n*Main Roles*\n-\n<@&631317435651325962>: __You help test our game, website, or Discord bot.__\n<@&631323469937770506>: __You have partnered with us as a Content Creator__\n<@&650854575364112405>: __You have partnered your own group with us__\n<@&707647379335020554>: __You are a verified member of Silver-Studios__\n<@&646420979370688512>: __You have won a giveaway before__\n<@&727590400046661694>: __You have signed up to get pinged for announcements__\n<@&733815838859329567>: __You are not verified__`);
         message.channel.send(`*Device Roles*\n-\n<@&631543640686067732>: __You mainly play on a gaming console__\n<@&631543559555645452>: __You mainly play on a mobile device__\n<@&631543445650931742>: __You mainly play on a desktop or laptop__\n\n*Region Roles*\n-\n<@&707954981348573259>: __You live in the US__\n<@&707955171640082432>: __You live in Europe__\n<@&707955415006183525>: __You live in an Oceania region__\n<@&707955552323633162>: __You live in South America__\n<@&707955059551371324>: __You live in Canada__\n<@&707955305908142116>: __You live in Asia__\n<@&707955454914986054>: __You live in Africa__\n<@&707955123174899806>: __You live in Mexico__\n\n:clipboard: **To apply for a role, please visit our website and fill out an application. Next, scroll down and give YOURSELF the relevant roles. Thanks, <@everyone>!**`);
@@ -233,10 +233,10 @@ bot.on("ready", () => {
 // Giveaway Feature
 
 bot.on('message', (message) => {
+    let prefix = '>';
     const ms = require('ms'); // npm install ms
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-    let prefix = '>';
     if (message.author.id !== '594371388228239370') return;
     if(message.channel.type !== 'text') return;
 
