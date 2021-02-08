c231acec2b924d45e7076d2c48c1f4ec25859d35
asilverstorm <78556714+asilverstorm@users.noreply.github.com>
2021-02-08 15:13:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -305,7 +305,7 @@ bot.on('messageDelete', async (message) => {
 
 // Moderation Commands
 
-bot.on('message', async (bot, message, args) => {
+bot.on('message', async (bot, message) => {
     const messageArray = message.content.split(' ');
 	const args = messageArray.slice(1);
     if(message.author.bot || message.channel.type === "dm") return;
