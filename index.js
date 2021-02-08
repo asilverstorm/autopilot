0f3137b0c85121127a2029d68239dcab2d2ca073
asilverstorm <78556714+asilverstorm@users.noreply.github.com>
2021-02-08 02:24:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ bot.on('message', async (message) => {
     if(message.channel.type !== 'text') return;
     let MessageArray = message.content.split(' ');
     let cmd = MessageArray[0].slice(settings.prefix.length);
-    let args = messageArray.slice(1);
+    let args = MessageArray.slice(1);
     if(!message.content.startsWith(settings.prefix)) return;
     
     if(cmd == 'links') {
