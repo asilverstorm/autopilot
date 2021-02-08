e10e525889e3b26e0a80f166a4973efddcb50ef2
asilverstorm <78556714+asilverstorm@users.noreply.github.com>
2021-02-07 17:20:13 -0800

    FUCK GO BACK

--- events/ready.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js")
-
-module.exports = bot => { 
-    console.log(`${bot.user.username} is online`)
-    bot.user.setActivity('Piloting Silver-Studios', {type: 'STREAMING', url: 'https://www.twitch.tv/asilverstorm880'});
-}
