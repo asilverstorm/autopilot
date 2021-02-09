fc696ebc5419bda80ec1be7d6ec09899e635686d
asilverstorm <78556714+asilverstorm@users.noreply.github.com>
2021-02-08 18:26:38 -0800

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ bot.on('message', async message => {
 
             let embed = new Discord.MessageEmbed()
             .setTitle('Bulk Messages Deleted')
-            .setDescription(`${args} Messages Deleted in ${message.author.channel}`)
+            .setDescription(`${args} Messages Deleted in ${message.channel}`)
             .setColor("ORANGE")
             .setTimestamp()
             bot.channels.cache.get(channelID).send(embed)
